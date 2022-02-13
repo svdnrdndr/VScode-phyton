0292acee5c0a7675379348698ce6104416b89b96
svdnrdndr <gunesbatarken35@gmail.com>
2022-02-13 10:59:21 +0300

    -m 011121-1425,

---
+++ 02_karar_yapilari/0201_karar_verme.py
@@ -0,0 +1,15 @@
+"""
+Lütfen 1. Sayı Giriniz : 25
+Lütfen 2. Sayı Giriniz : 15
+25>15
+"""
+"""
+l. bir s . giriniz : -9
+mutlak değer 9
+"""
+"""
+sayi = int(input("l. bir s . giriniz\t: "))
+if sayi < 0:
+    sayi= sayi*(-1)
+print(f"sayının mutlak değeri {sayi}") 
+#sayının mutlak değeri 9 dur."""
